19a0a4bcb2ff2cbf7c31a3a423abc54b58c00128
shakti sumant das <shakti.20k@gmail.com>
2025-02-06 14:40:12 +0530

    create env file and use in firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyADRSBPykEMTUdbdmlH56dr8JhTM_MET_4",
-  authDomain: "auth-assignment-a904f.firebaseapp.com",
-  projectId: "auth-assignment-a904f",
-  storageBucket: "auth-assignment-a904f.firebasestorage.app",
-  messagingSenderId: "120563383442",
-  appId: "1:120563383442:web:c9d327aaf10deb6655fa4b"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
